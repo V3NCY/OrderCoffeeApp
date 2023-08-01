554adf51e2b74de9b37a06151848c252f00c517d
unknown <venceslavam@gmail.com>
2023-08-01 14:48:13 +0300

    initial push

---
+++ OrderCoffeeApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace OrderCoffeeApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ OrderCoffeeApp/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace OrderCoffeeApp;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
